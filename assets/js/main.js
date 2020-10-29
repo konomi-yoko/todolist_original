073959bf54471771e4ad1fd41e60b3820862e8da
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-29 17:09:03 +0900

    liを追加してみたい

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 // このファイルに処理を記述する
+
+let addBtn = document.querySelector('#btn');
+
+addBtn.addEventListener('click',function(){
+
+let input_text = document.querySelector('#input').value;
+
+if (input_text != ''){
+  let todo_list = document.querySelector('.todo-list');
+
+  let li = document.createElement('li');
+  li.classList.add('list');
+
+  li.textContent = input_text;
+  console.log(li);
+
+  // let div_delete = document.createElement('div');
+
+  // div_delete.classList.add('delete');
+  // div_delete.textContent = 'Delete';
+  // div_delete.addEventListener('click',function(){
+  //   let hantei = confirm('本当に削除しますか？');
+  //   if (hantei == true){
+  //         this.parentElement.remove();
+  //   }
+  // });
+
+
+  // li.appendChild(div_delete);
+  todo_list.appendChild(li);
+
+  // document.querySelector('#input').value = '';
+}
+
+});
