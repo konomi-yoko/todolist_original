38bdc4f2a8b74ed3b13dacd179376484ffb06432
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-10-31 11:18:57 +0900

    ある程度

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+let data;
+
+if (localStorage.getItem('todoList')) {
+  data = JSON.parse(localStorage.getItem('todoList'));
+}else {
+  data = [];
+}
+
+for (let displayText of data){
+  add_li_tag(displayText);
+}
+
 let addBtn = document.querySelector('#btn');
 
 addBtn.addEventListener('click',function(){
@@ -5,30 +17,50 @@ addBtn.addEventListener('click',function(){
 let input_text = document.querySelector('#input').value;
 
 if (input_text != ''){
-  let todo_list = document.querySelector('.todo-list');
 
-  let li = document.createElement('li');
-  li.classList.add('list');
+  add_li_tag(input_text);
 
-  li.textContent = input_text;
-  console.log(li);
+data.push(input_text);
 
-  let span_delete = document.createElement('span');
-
-  span_delete.classList.add('delete');
-  // span_delete.textContent = 'Delete';
-  span_delete.addEventListener('click',function(){
-    let hantei = confirm('本当に削除しますか？');
-    if (hantei == true){
-          this.parentElement.remove();
-    }
-  });
-
-
-  li.appendChild(span_delete);
-  todo_list.appendChild(li);
+localStorage.setItem('todoList',JSON.stringify(data));
 
   document.querySelector('#input').value = '';
 }
 
 });
+
+
+function add_li_tag(task_text){
+
+  let todo_list = document.querySelector('.todo-list');
+
+  let li = document.createElement('li');
+  li.classList.add('list');
+
+  li.textContent = task_text;
+  console.log(li);
+
+  let div_delete = document.createElement('div');
+
+  div_delete.classList.add('delete');
+  div_delete.addEventListener('click',function(){
+    let hantei = confirm('本当に削除しますか？');
+   if (hantei == true){
+          let del_text = this.parentElement.textContent;
+          del_text = del_text.replace('Delete','')
+          data.splice(data.indexOf(del_text),1);
+
+          this.parentElement.remove();
+          localStorage.setItem('todoList',JSON.stringify(data));
+    }
+  });
+
+let i_delete = document.createElement('i');
+i_delete.classList.add('fas');
+i_delete.classList.add('fa-trash');
+div_delete.appendChild(i_delete);
+
+  li.appendChild(div_delete);
+  todo_list.appendChild(li);
+
+}
